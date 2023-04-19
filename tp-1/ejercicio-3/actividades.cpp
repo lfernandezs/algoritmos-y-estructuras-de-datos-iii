47b4009a42ef8cfe52ce7c1dbb2de5ed470eb7c8
cchumacero <74564482+cchumacero@users.noreply.github.com>
2023-04-19 14:50:37 -0300

    Ejercicio 3 - Mejora de tiempo

--- tp-1/ejercicio-3/actividades.cpp
+++ tp-1/ejercicio-3/actividades.cpp
@@ -1,57 +1,59 @@
-#include <iostream>
-#include <vector>
-#include <tuple>
-using namespace std;
-
-int main()
-{
-    int n, s, t;
-    vector<tuple<int, int, int> > S;
-
-    cin >> n;
-    vector<vector<tuple<int, int, int> >> orden((2*n)+1);
-    for(int i = 0; i < n; i++)
-    {
-        cin >> s >> t;
-        orden[t].push_back({i+1, s, t});
-    }
-
-    for(int i = 1; i <= 2*n; i++)
-    {
-        if(orden[i].size() > 0)
+        #include <iostream>
+        #include <stdio.h>
+        #include <vector>
+        #include <tuple>
+        using namespace std;
+         
+        int main()
         {
-            if(S.size() == 0)
+            int n, s, t;
+            vector<tuple<int, int, int> > S;
+         
+            scanf("%d", &n);
+            int maximo = -1, minimo = 2*n;
+            vector<vector<tuple<int, int, int> >> orden((2*n)+1);
+            for(int i = 0; i < n; i++)
             {
-                int minimo = (2*n)+1, minPos = -1;
-                for(int j = 0; j < orden[i].size(); j++)
+                scanf("%d", &s);
+                scanf("%d", &t);
+                orden[t].push_back({i+1, s, t});
+            }
+            
+            for(int i = 1; i <= 2*n; i++)
+            {
+                if(orden[i].size() > 0)
                 {
-                    if(get<1>(orden[i][j]) < minimo)
+                    if(S.size() == 0)
                     {
-                        minimo = get<1>(orden[i][j]);
-                        minPos = j;
+                        int minimo = (2*n)+1, minPos = -1;
+                        for(int j = 0; j < orden[i].size(); j++)
+                        {
+                            if(get<1>(orden[i][j]) < minimo)
+                            {
+                                minimo = get<1>(orden[i][j]);
+                                minPos = j;
+                            }
+                        }
+                        S.push_back(orden[i][minPos]);
+                    }
+                    else
+                    {
+                        int minimo = (2*n)+1, minPos = -1;
+                        for(int j = 0; j < orden[i].size(); j++)
+                        {
+                            if(get<1>(orden[i][j]) < minimo && get<1>(orden[i][j]) >= get<2>(S[S.size() - 1]))
+                            {
+                                minimo = get<1>(orden[i][j]);
+                                minPos = j;
+                            }
+                        }
+                        if(minPos != -1) S.push_back(orden[i][minPos]);
                     }
                 }
-                S.push_back(orden[i][minPos]);
             }
-            else
-            {
-                int minimo = (2*n)+1, minPos = -1;
-                for(int j = 0; j < orden[i].size(); j++)
-                {
-                    if(get<1>(orden[i][j]) < minimo && get<1>(orden[i][j]) >= get<2>(S[S.size() - 1]))
-                    {
-                        minimo = get<1>(orden[i][j]);
-                        minPos = j;
-                    }
-                }
-                if(minPos != -1) S.push_back(orden[i][minPos]);
-            }
-
-        }
-    }
-
-    cout << S.size() << endl;
-    for(auto i : S) cout << get<0>(i) << " ";
-
-    return 0;
-}
+            
+            printf("%d\n", S.size());
+            for(auto i : S) printf("%d ", get<0>(i));
+            
+            return 0;
+        }  
