14116739204365db453a002d5203270c047673ed
cchumacero <cnchumacero@gmail.com>
2023-05-18 12:25:12 -0300

    Se agrega la solucion del ej3

--- tp-2/ej3/modems.cpp
+++ tp-2/ej3/modems.cpp
@@ -8,15 +8,11 @@ using namespace std;
 using ll = long long;
 const ll inf = 1e18;
 
-int n, limit, modems, u, v;
-vector<tuple<ll,int,int, int>> E;
-pair<int, int> resultados[1001];
+int n, limit, modems, cu, cv;
+vector<tuple<double,int,int, int>> E;
 struct DSU{
 
-    DSU(int n){
-        padre = rank = vector<int>(n+1);
-        for(int v = 1; v <= n; v++) padre[v] = v;
-    }
+    DSU(int n){ for(int v = 0; v < n; v++) padre[v] = v;}
 
     int find(int v){
         if(v == padre[v]) return v;
@@ -30,40 +26,30 @@ struct DSU{
         padre[v] = padre[u];
         rank[u] = max(rank[u],rank[v]+1);
     }
-
-    vector<int> padre;
-    vector<int> rank;
+    int padre[1001];
+    int rank[1001] = {0};
 };
 
 void kruskal(int caso){
     sort(E.begin(),E.end());
     int componentes = n;
-    ll resU = 0, resV = 0;
-    int aristas = 0;
+    double resU = 0, resV = 0;
     DSU dsu(n);
     for(auto [c,u,v,t] : E){
         //si (u,v) es arista segura
         if(dsu.find(u) != dsu.find(v)){
-            // agregar
             dsu.unite(u,v);
-            //aristas++;
-            componentes--;
             if(t == 0) resU += c; else resV += c;
+            componentes--;
+            if(componentes == modems) break;
         }
-        if(componentes == modems) break;
     }
-    /*
-    if(aristas == n-1) cout<<res<<endl;
-    else cout<<"IMPOSSIBLE\n";
-    */
-    //printf("Caso #%d: %.3f %.3f\n", caso, float(resU), float(resV));
-    resultados[caso].first = resU;
-    resultados[caso].second = resV;
+    printf("Caso #%d: %.3f %.3f\n", caso, resU, resV);
 }
 
-int dist(pair<int,int> a, pair<int,int>b)
+double dist(pair<int,int> a, pair<int,int>b)
 {
-    return sqrt((b.x - a.x)*(b.x - a.x) + (b.y - a.y)*(b.y - a.y));
+    return (double)sqrt((b.x - a.x)*(b.x - a.x)+(b.y - a.y)*(b.y - a.y));
 }
 int main() {
     int t, c = 0;
@@ -71,39 +57,26 @@ int main() {
     while(c++ < t)
     {
         pair<int,int> coordenadas[1001];
-        scanf("%d%d%d%d%d", &n, &limit, &modems, &u, &v);
+        scanf("%d%d%d%d%d", &n, &limit, &modems, &cu, &cv);
         for(int i = 0; i < n; i++)
         {
             int a, b;
-            cin >> a >> b;
-            coordenadas[i+1].x = a;
-            coordenadas[i+1].y = b;
+            scanf("%d%d", &a, &b);
+            coordenadas[i] = {a,b};
         }
         //crear aristas
-        for(int i = 1; i <= n; i++)
+        for(int i = 0; i < n; i++)
         {
-            for(int j = 1; j <= n; j++)
+            for(int j = i + 1; j < n; j++)
             {
-                if(i != j)
-                {
-                    int distancia = dist(coordenadas[i], coordenadas[j]);
-                    if(distancia > limit)
-                        E.push_back({distancia*v, i, j, 1});
-                    else
-                    {
-                        if(u <= v)
-                            E.push_back({distancia * u, i, j, 0});
-                        else E.push_back({distancia * v, i, j, 1});
-                    }
-                }
+                double d = dist(coordenadas[i], coordenadas[j]);
+                if(d > limit)
+                    E.push_back({d*cv, i, j, 1});
+                else E.push_back({d*cu, i, j, 0});
             }
         }
         kruskal(c);
         E.clear();
-    }
-    for(int i = 1;i <= t; i++)
-    {
-        printf("Caso #%d: %.3f %.3f\n", i, float(resultados[i].first), float(resultados[i].second));
     }
     return 0;
 }
