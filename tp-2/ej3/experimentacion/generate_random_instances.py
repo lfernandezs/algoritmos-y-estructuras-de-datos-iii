671e06e186f0cb99dccc15cb9de2567b59b361cf
Luciana Fernández Spandau <fernandezspandau@gmail.com>
2023-05-26 13:34:08 -0300

    arbol camino

--- tp-2/ej3/experimentacion/generate_random_instances.py
+++ tp-2/ej3/experimentacion/generate_random_instances.py
@@ -1,7 +1,7 @@
 from random import randint
 MAX_CANT_OFICINAS = 1000
 MAX_DIST_UTP = 10000
-INSTANCES_PER_TRY = 20
+INSTANCES_PER_TRY = 1
 
 
 def generate_random_instances():
@@ -14,14 +14,15 @@ def generate_random_instances():
             f.write(str(i) + " ")
             dist_utp = randint(1, MAX_DIST_UTP)
             modems = randint(1, MAX_CANT_OFICINAS-1)
-            precio_utp = randint(1, 10)
+            precio_utp = randint(1, 9)
             precio_fibra = randint(precio_utp, 10)
             f.write(str(dist_utp) + " ")
             f.write(str(modems) + " ")
             f.write(str(precio_utp) + " ")
             f.write(str(precio_fibra) + " ")
+            oficina = [1, 1]
             for k in range(i):
-                oficina = (randint(0, MAX_DIST_UTP), randint(0, MAX_DIST_UTP))
+                oficina = (1, oficina[1]+2*k)
                 f.write(str(oficina[0]) + " " + str(oficina[1]) + " ")
             f.write("\n")
 
